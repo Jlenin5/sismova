c0471d0e0142ec089dbc3159df596934795b2904
Jlenin5 <jlenin25mv@gmail.com>
2024-03-09 17:28:19 -0500

    user_navbar_header

--- src/app/theme-layouts/shared-components/UserNavbarHeader.js
+++ src/app/theme-layouts/shared-components/UserNavbarHeader.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography'
 import { useSelector } from 'react-redux'
 import { selectUser } from 'app/store/userSlice'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
 
 const Root = styled('div')(({ theme }) => ({
   '& .username, & .email': {
@@ -28,30 +26,9 @@ const Root = styled('div')(({ theme }) => ({
 }))
 
 function UserNavbarHeader(props) {
-  const [dataEmployee,setDataEmployee] = useState([])
-  const [dataAvatar,setDataAvatar] = useState([])
   const user = useSelector(selectUser)
 
-  const url = 'https://sismova.tech/backsis/public/api/'
-
-  const getEmployee = async() => {
-    const response = await axios.get(url+'emp')
-    return response.data
-  }
-
-  const getAvatar = async() => {
-    const response = await axios.get(url+'ava')
-    return response.data
-  }
-
-  useEffect(() => {
-    getEmployee().then(r => setDataEmployee(r))
-    getAvatar().then(r => setDataAvatar(r))
-  }, [])
-
-  
-  const filtHr = dataEmployee.find(r => r.id === user.Employee)
-  const filtAva = dataAvatar.find(r => r.id === user.Avatar)
+  const avatar = user.employees.avatars.avaName
 
   return (
     <Root className="user relative flex flex-col items-center justify-center p-16 pb-14 shadow-0">
@@ -62,7 +39,7 @@ function UserNavbarHeader(props) {
             color: 'text.secondary',
           }}
           className="avatar text-32 font-bold w-96 h-96"
-          src={`https://sismova.tech/backsis/public/images/avatars/${filtAva ? filtAva.avaName : 'nocamera.png'}`}
+          src={`https://sismova.tech/backsis/public/images/avatars/${avatar ? avatar : 'nocamera.png'}`}
           alt={user.userDisplayName}
         >
           {user.userDisplayName.charAt(0)}
@@ -72,7 +49,7 @@ function UserNavbarHeader(props) {
         {user.userDisplayName}
       </Typography>
       <Typography className="email text-13 whitespace-nowrap font-medium" color="text.secondary">
-        {filtHr ? filtHr.empEmail : ''}
+        {user.employees.empEmail}
       </Typography>
     </Root>
   )
