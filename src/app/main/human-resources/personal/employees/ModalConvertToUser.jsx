ffa69435e2d2dcd2ffcfdefa7575681cccf9aaf7
Jlenin5 <jlenin25mv@gmail.com>
2024-08-01 21:57:45 -0500

    create_user_from_employee

--- src/app/main/human-resources/personal/employees/ModalConvertToUser.jsx
+++ src/app/main/human-resources/personal/employees/ModalConvertToUser.jsx
@@ -1,3 +1,4 @@
+import FuseUtils from '@fuse/utils'
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField } from '@mui/material'
 import Typography from '@mui/material/Typography'
 import FormControl from '@mui/material/FormControl'
@@ -6,13 +7,14 @@ import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import CloseIcon from '@mui/icons-material/Close'
+import IconButton from '@mui/material/IconButton'
 import { useTranslation } from 'react-i18next'
 import { postUser } from '../store/userSlice'
 import { getRoles } from 'src/app/main/settings/controls/store/rolSlice'
 
 const ModalConvertToUser = ({open, close, emp}) => {
   const dispatch = useDispatch()
-  // const [emp, setEmp] = useState({})
   const [rol, setRol] = useState(1)
   const [dRol, setDRol] = useState([])
   const { t } = useTranslation()
@@ -44,7 +46,7 @@ const ModalConvertToUser = ({open, close, emp}) => {
   const surnameLowerCase = emp?.surname.toLowerCase() ?? ''
   const surnameReplace = surnameLowerCase.replace(/\s+/g, '')
   const newEmail = nameReplace + '.' + surnameReplace + '@sismova.tech'
-  const newNickName = emp?.first_name.substr(0,3) + calculateAge(emp?.birthdate)
+  const newNickName = emp?.first_name.substr(0,3) + (emp?.birthdate ? calculateAge(emp?.birthdate) : '')
 
   const handleSubmit = () => {
     dispatch(postUser({
@@ -52,27 +54,33 @@ const ModalConvertToUser = ({open, close, emp}) => {
       nickname: emp?.first_name,
       email: newEmail,
       password: nameReplace + '.' + surnameReplace,
-      uuid: '',
+      uuid: FuseUtils.generateGUID(),
       status: 1
     }))
+    close()
   }
-  console.log(emp)
 
   useEffect(() => {
     if(emp) {
-      // dispatch(getEmployee(Number(emp))).then(r => setEmp(r.payload))
       // dispatch(getRoles()).then(r => setDRol(r.payload))
     }
   }, [dispatch, emp])
 
   return (
-    <Dialog
-      onClose={handleClose}
-      open={open}
-      className='form-dialog-category'
-    >
-      <DialogTitle>{t('user_data')}</DialogTitle>
-      <DialogContent className='grid grid-flow-row-dense grid-cols-1 gap-32 mt-12'>
+    <Dialog open={open} className="form-dialog-category">
+      <DialogTitle className="flex justify-between mt-10">
+        <div>{t('user_data')}</div>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent className='grid grid-flow-row-dense grid-cols-1 gap-32' dividers>
         <div className="name">
           <Typography variant="h6" gutterBottom className="text-red">
             {t('user_name')}:
@@ -87,14 +95,6 @@ const ModalConvertToUser = ({open, close, emp}) => {
           </Typography>
           <Typography variant="h6" gutterBottom>
             {newEmail}
-          </Typography>
-        </div>
-        <div className="n_document">
-          <Typography variant="h6" gutterBottom className="text-red">
-            {t('n_document')}:
-          </Typography>
-          <Typography variant="h6" gutterBottom>
-            {emp?.document_number}
           </Typography>
         </div>
         <div className="password">
@@ -120,9 +120,9 @@ const ModalConvertToUser = ({open, close, emp}) => {
           </Select>
         </FormControl> */}
       </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>{t('cancel')}</Button>
-        <Button onClick={handleSubmit}>{t('convert')}</Button>
+      <DialogActions className="m-10">
+        <Button variant="contained" color="error" onClick={handleClose}>{t('cancel')}</Button>
+        <Button variant="contained" color="success" onClick={handleSubmit}>{t('convert')}</Button>
       </DialogActions>
     </Dialog>
   )
--- src/app/main/human-resources/personal/employees/OptionsAction.jsx
+++ src/app/main/human-resources/personal/employees/OptionsAction.jsx
@@ -1,11 +1,9 @@
 import { useTranslation } from 'react-i18next'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
-import { useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import ModalConvertToUser from './ModalConvertToUser'
-import ShowDetails from './ShowDetails'
 import { deleteEmployee } from '../store/employeesSlice'
 import EmployeeForm from './EmployeeForm'
 
@@ -13,21 +11,15 @@ const ITEM_HEIGHT = 48
 
 function OptionsAction(props) {
   const dispatch = useDispatch()
-  const navigate = useNavigate()
   const [openEditEmployee, setOpenEditEmployee] = useState(false)
   const [openCovertUser, setOpenConvertUser] = useState(false)
-  const [openShowDetails, setOpenShowDetails] = useState(false)
   const { t } = useTranslation()
-
-  // const editEmployee = () => {
-  //   handleClose()
-  //   navigate(`/human-resources/personal/employee/${idE}`)
-  // }
 
   const openModalEditEmployee = () => {
     handleClose()
     setOpenEditEmployee(true)
   }
+
   const closeModalEditEmployee = () => {
     props.fetchData(props.page, props.rowsPerPage, '')
     setOpenEditEmployee(false)
@@ -37,18 +29,10 @@ function OptionsAction(props) {
     handleClose()
     setOpenConvertUser(true)
   }
+  
   const closeModalConvertUser = () => {
     props.fetchData(props.page, props.rowsPerPage, '')
     setOpenConvertUser(false)
-  }
-
-  const openModalShowDetails = () => {
-    handleClose()
-    setOpenShowDetails(true)
-  }
-  const closeModalShowDetails = () => {
-    props.fetchData(props.page, props.rowsPerPage, '')
-    setOpenShowDetails(false)
   }
 
   const handleClose = () => {
@@ -81,12 +65,12 @@ function OptionsAction(props) {
         }}>
           {t('delete')}
         </MenuItem>
-        <MenuItem onClick={openModalShowDetails}>
-          {t('show_details')}
-        </MenuItem>
-        <MenuItem onClick={openModalConvertUser}>
-          {t('convert_to_user')}
-        </MenuItem>
+        {
+          props.idE?.user ? '' :
+          <MenuItem onClick={openModalConvertUser}>
+            {t('convert_to_user')}
+          </MenuItem>
+        }
       </Menu>
       <ModalConvertToUser
         open={openCovertUser}
@@ -100,11 +84,6 @@ function OptionsAction(props) {
         dataToEdit={props.dataToEdit}
         setDataToEdit={props.setDataToEdit}
       />
-      {/* <ShowDetails
-        open={openShowDetails}
-        close={closeModalShowDetails}
-        idE={idE}
-      /> */}
     </>
   )
 }
